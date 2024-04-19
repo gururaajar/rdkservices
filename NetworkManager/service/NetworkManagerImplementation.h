88fbeb0bc9cdcd1699b22142d1d8e164524d5672
Gururaaja E S R <Gururaja_ErodeSriranganRamlingham@comcast.com>
2024-04-19 21:06:16 +0000

    To test INetworkManager in wpeframework

--- NetworkManager/service/NetworkManagerImplementation.h
+++ NetworkManager/service/NetworkManagerImplementation.h
@@ -28,8 +28,8 @@
 
 using namespace std;
 
-//#include <interfaces/INetworkManager.h>
-#include "INetworkManager.h"
+#include <interfaces/INetworkManager.h>
+//#include "INetworkManager.h"
 #include "NetworkManagerLogger.h"
 #include "WifiSignalStrengthMonitor.h"
 #include "NetworkConnectivity.h"
